a41d848df2989308d93204e9b2534e4c1e5399c1
loukylor <46853114+loukylor@users.noreply.github.com>
2021-03-01 22:48:39 -0800

    Add CloningBeGone

---
+++ CloningBeGone/CloningBeGone.cs
@@ -0,0 +1,16 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(CloningBeGone.CloningBeGoneMod), "CloningBeGone", "1.0.1", "loukylor (https://github.com/loukylor/CloningBeGone)")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+namespace CloningBeGone
+{
+    class CloningBeGoneMod : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (buildIndex == -1) GameObject.Find("UserInterface/MenuContent/Screens/Settings/OtherOptionsPanel/AllowAvatarCopyingToggle").GetComponent<UiSettingConfig>().SetEnable(false);
+        }
+    }
+}
