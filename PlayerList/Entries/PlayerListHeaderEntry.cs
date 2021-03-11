2fc6114e7d00a94a31df5d2bd4995c7f48d02826
loukylor <46853114+loukylor@users.noreply.github.com>
2021-03-11 13:11:33 -0800

    Delete PlayerList directory

--- PlayerList/Entries/PlayerListHeaderEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PlayerList.Entries
-{
-    class PlayerListHeaderEntry : EntryBase
-    {
-        public override string Name { get { return "PlayerList Header"; } }
-
-        public override void ProcessText(object[] parameters = null) => ChangeEntry("playercount", PlayerListMod.playerEntries.Count + 1);
-    }
-}
--- PlayerList/Entries/WorldAuthorEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PlayerList.Entries
-{
-    class WorldAuthorEntry : EntryBase
-    {
-        public override string Name { get { return "World Author"; } }
-
-        public override void ProcessText(object[] parameters = null) => ChangeEntry("worldauthor", RoomManager.field_Internal_Static_ApiWorld_0.authorName);
-    }
-}
