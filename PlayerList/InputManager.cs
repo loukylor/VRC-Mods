547d42a30835098af8e417d7ead74b7a2a952ad8
loukylor <Loukylor@users.noreply.github.com>
2021-04-02 21:55:59 -0700

    Forgot to commit this oops

--- PlayerList/InputManager.cs
+++ PlayerList/InputManager.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using System.Reflection;
+using UnityEngine;
 
 namespace PlayerList
 {
     class InputManager
     {
+        public static MethodInfo clickMethod;
         public static VRCUiCursor CurrentCursor
         {
             get { return VRCUiCursorManager.Method_Public_Static_VRCUiCursor_0(); }
@@ -28,7 +31,10 @@ namespace PlayerList
 
         public static void SelectPlayer(VRCPlayer player)
         {
-            CurrentCursor.Method_Public_Void_VRCPlayer_PDM_0(player);
+            if (clickMethod == null)
+                clickMethod = typeof(VRCUiCursor).GetMethods().First(mi => mi.Name.StartsWith("Method_Public_Void_VRCPlayer_") && mi.GetParameters().Any(pi => pi.ParameterType == typeof(VRCPlayer)));
+
+            clickMethod.Invoke(CurrentCursor, new object[] { player });
         }
     }
 }
--- PlayerList/MenuManager.cs
+++ PlayerList/MenuManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Harmony;
 using MelonLoader;
 using PlayerList.Components;
 using PlayerList.Entries;
