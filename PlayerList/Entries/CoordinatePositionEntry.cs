f8fc44b98c3907be6ab2f31e8a1bb749c1ca3c77
loukylor <Loukylor@users.noreply.github.com>
2021-04-23 15:43:19 -0700

    Somehow this specific entry took the most frame time (literally 4 times the frametime of all the other components) so i made it take less

--- PlayerList/Entries/CoordinatePositionEntry.cs
+++ PlayerList/Entries/CoordinatePositionEntry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 using VRC;
 
 namespace PlayerList.Entries
@@ -6,12 +7,21 @@ namespace PlayerList.Entries
     class CoordinatePositionEntry : EntryBase
     {
         public override string Name { get { return "Coordinate Position"; } }
+
+        private Vector3Int lastPos;
+
         protected override void ProcessText(object[] parameters = null)
         {
-            ResetEntry();
-            ChangeEntry("x", Math.Round(Player.prop_Player_0.gameObject.transform.position.x, 1));
-            ChangeEntry("y", Math.Round(Player.prop_Player_0.gameObject.transform.position.y, 1));
-            ChangeEntry("z", Math.Round(Player.prop_Player_0.gameObject.transform.position.z, 1));
+            Vector3Int pos = Vector3Int.RoundToInt(Player.prop_Player_0.gameObject.transform.position);
+
+            if (pos == lastPos) return;
+            lastPos = pos;
+
+            string tempString = OriginalText;
+            tempString = tempString.Replace("{x}", Math.Round(Player.prop_Player_0.gameObject.transform.position.x, 1).ToString());
+            tempString = tempString.Replace("{y}", Math.Round(Player.prop_Player_0.gameObject.transform.position.y, 1).ToString());
+            tempString = tempString.Replace("{z}", Math.Round(Player.prop_Player_0.gameObject.transform.position.z, 1).ToString());
+            textComponent.text = tempString;
         }
     }
 }
