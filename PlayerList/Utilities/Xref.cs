3473283f4d795cb2024b2003bc8bbc743e78676e
loukylor <Loukylor@users.noreply.github.com>
2021-05-17 14:12:00 -0700

    Cleanup Xref scan utils - PL

--- PlayerList/Utilities/Xref.cs
+++ PlayerList/Utilities/Xref.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Reflection;
+using UnhollowerRuntimeLib.XrefScans;
 
 namespace PlayerList.Utilities
 {
@@ -11,8 +12,8 @@ namespace PlayerList.Utilities
         {
             try
             {
-                return UnhollowerRuntimeLib.XrefScans.XrefScanner.XrefScan(method)
-                    .Where(instance => instance.Type == UnhollowerRuntimeLib.XrefScans.XrefType.Global && instance.ReadAsObject().ToString().Contains(match)).Any();
+                return XrefScanner.XrefScan(method)
+                    .Where(instance => instance.Type == XrefType.Global && instance.ReadAsObject().ToString().Contains(match)).Any();
             }
             catch { }
             return false;
@@ -21,7 +22,7 @@ namespace PlayerList.Utilities
         {
             try
             {
-                return UnhollowerRuntimeLib.XrefScans.XrefScanner.UsedBy(method)
+                return XrefScanner.UsedBy(method)
                     .Where(instance => instance.TryResolve() != null && instance.TryResolve().Name.Contains(methodName)).Any();
             }
             catch { }
@@ -29,8 +30,8 @@ namespace PlayerList.Utilities
         }
         public static bool CheckUsing(MethodInfo method, string match, Type type)
         {
-            foreach (UnhollowerRuntimeLib.XrefScans.XrefInstance instance in UnhollowerRuntimeLib.XrefScans.XrefScanner.XrefScan(method))
-                if (instance.Type == UnhollowerRuntimeLib.XrefScans.XrefType.Method)
+            foreach (XrefInstance instance in XrefScanner.XrefScan(method))
+                if (instance.Type == XrefType.Method)
                     try
                     {
                         if (instance.TryResolve().DeclaringType == type && instance.TryResolve().Name.Contains(match))
