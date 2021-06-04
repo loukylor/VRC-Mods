36b75d1d21cdc61649c6c2265bf315f78a9cb17a
loukylor <Loukylor@users.noreply.github.com>
2021-06-03 20:54:02 -0700

    Version bump

--- SelectYourself/SelectYourselfMod.cs
+++ SelectYourself/SelectYourselfMod.cs
@@ -7,7 +7,7 @@ using UIExpansionKit.API;
 using UnityEngine;
 using VRC.Core;
 
-[assembly: MelonInfo(typeof(SelectYourself.SelectYourselfMod), "SelectYourself", "1.0.0", "loukylor", "https://github.com/loukylor/VRC-Mods")]
+[assembly: MelonInfo(typeof(SelectYourself.SelectYourselfMod), "SelectYourself", "1.0.1", "loukylor", "https://github.com/loukylor/VRC-Mods")]
 [assembly: MelonGame("VRChat", "VRChat")]
 
 namespace SelectYourself
