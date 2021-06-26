a4b31ba255c732985bd6310be766a26d82c5152e
loukylor <Loukylor@users.noreply.github.com>
2021-06-26 12:55:06 -0700

    Update SY to require VRChatUtilityKit

--- SelectYourself/SelectYourselfMod.cs
+++ SelectYourself/SelectYourselfMod.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using MelonLoader;
 using UIExpansionKit.API;
 using UnityEngine;
 using VRC;
-using VRC.Core;
+using VRChatUtilityKit.Ui;
 
-[assembly: MelonInfo(typeof(SelectYourself.SelectYourselfMod), "SelectYourself", "1.0.3", "loukylor", "https://github.com/loukylor/VRC-Mods")]
+[assembly: MelonInfo(typeof(SelectYourself.SelectYourselfMod), "SelectYourself", "1.0.4", "loukylor", "https://github.com/loukylor/VRC-Mods")]
 [assembly: MelonGame("VRChat", "VRChat")]
 
 namespace SelectYourself
@@ -21,22 +18,13 @@ namespace SelectYourself
         {
             MelonPreferences_Category category = MelonPreferences.CreateCategory("SelectYourself", "SelectYourself Settings");
             selectYourselfPref = category.CreateEntry(nameof(selectYourselfPref), true, "Enable/Disable Select Yourself Button");
+            selectYourselfPref.OnValueChangedUntyped += OnPrefChange;
 
-            List<Type> quickMenuNestedEnums = typeof(QuickMenu).GetNestedTypes().Where(type => type.IsEnum).ToList();
-            PropertyInfo quickMenuEnumProperty = typeof(QuickMenu).GetProperties()
-                .First(pi => pi.PropertyType.IsEnum && quickMenuNestedEnums.Contains(pi.PropertyType));
-            MethodInfo setMenuIndex = typeof(QuickMenu).GetMethods()
-                .First(mb => mb.Name.StartsWith("Method_Public_Void_Enum") && !mb.Name.Contains("_PDM_") && mb.GetParameters()[0].ParameterType == quickMenuEnumProperty.PropertyType);
             ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddSimpleButton("Select Yourself",
-                new Action(() => 
-                {
-                    QuickMenu.prop_QuickMenu_0.field_Private_Player_0 = Player.prop_Player_0;
-                    QuickMenu.prop_QuickMenu_0.prop_APIUser_0 = APIUser.CurrentUser;
-                    setMenuIndex.Invoke(QuickMenu.prop_QuickMenu_0, new object[1] { 3 });
-                }),
+                new Action(() => UiManager.OpenUserInQuickMenu(Player.prop_Player_0)),
                 new Action<GameObject>((gameObject) => selectYourselfButton = gameObject));
         }
-        public override void OnPreferencesSaved()
+        public static void OnPrefChange()
         {
             selectYourselfButton?.SetActive(selectYourselfPref.Value);
         }
