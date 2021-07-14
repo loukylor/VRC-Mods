bf8702d50119ec3c6aac7289eac19d523ceda1ec
loukylor <Loukylor@users.noreply.github.com>
2021-07-14 13:52:24 -0700

    Cleanup + fix the OnModerationSent again - VRCUK

--- VRChatUtilityKit/Ui/Label.cs
+++ VRChatUtilityKit/Ui/Label.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
@@ -9,12 +10,12 @@ namespace VRChatUtilityKit.Ui
         public Label(string parent, Vector3 position, string text, string labelName, int fontSize = 72, bool resize = false, Color? textColor = null) : this(GameObject.Find(parent), position, text, labelName, fontSize, resize, textColor) { }
         public Label(GameObject parent, Vector3 position, string text, string labelName, int fontSize = 72, bool resize = false, Color? textColor = null) : base(parent, GameObject.Find("UserInterface/QuickMenu/UIElementsMenu/NameplatesOnButton"), position, text, labelName, resize, textColor)
         {
-            Object.DestroyImmediate(gameObject.GetComponent<UnityEngine.UI.Button>());
+            Object.DestroyImmediate(gameObject.GetComponent<Button>());
             Object.DestroyImmediate(gameObject.GetComponent<UiTooltip>());
-            Object.DestroyImmediate(gameObject.GetComponent<UnityEngine.UI.Image>());
+            Object.DestroyImmediate(gameObject.GetComponent<Image>());
             
-            TextComponent = gameObject.AddComponent<UnityEngine.UI.Text>();
-            TextComponent.font = gameObject.transform.GetChild(0).GetComponent<UnityEngine.UI.Text>().font;
+            TextComponent = gameObject.AddComponent<Text>();
+            TextComponent.font = gameObject.transform.GetChild(0).GetComponent<Text>().font;
             TextComponent.fontSize = fontSize;
             TextComponent.alignment = TextAnchor.MiddleCenter;
             TextComponent.text = text;
