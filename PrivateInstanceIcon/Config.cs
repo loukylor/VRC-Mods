b9650ee8be25285f9b319a7a2df61318a2ad7838
ljoonal <ljoonal@ljoonal.xyz>
2021-09-02 22:38:39 +0300

    Admit defeat & register prefs as strings for now

--- PrivateInstanceIcon/Config.cs
+++ PrivateInstanceIcon/Config.cs
@@ -1,3 +1,4 @@
+using System;
 using MelonLoader;
 using UIExpansionKit.API;
 using VRChatUtilityKit.Utilities;
@@ -15,23 +16,28 @@ namespace PrivateInstanceIcon
     {
         public static InstanceBehavior PrivateInstanceBehavior
         {
-            get { return privateInstanceBehavior.Value; }
+            //get { return privateInstanceBehavior.Value; }
+            get { return (InstanceBehavior)Enum.Parse(typeof(InstanceBehavior), privateInstanceBehavior.Value); }
         }
         public static InstanceBehavior JoinablePrivateInstanceBehavior
         {
-            get { return joinablePrivateInstanceBehavior.Value; }
+            //get { return joinablePrivateInstanceBehavior.Value; }
+            get { return (InstanceBehavior)Enum.Parse(typeof(InstanceBehavior), joinablePrivateInstanceBehavior.Value); }
         }
         public static InstanceBehavior FriendsInstanceBehavior
         {
-            get { return friendsInstanceBehavior.Value; }
+            //get { return friendsInstanceBehavior.Value; }
+            get { return (InstanceBehavior)Enum.Parse(typeof(InstanceBehavior), friendsInstanceBehavior.Value); }
         }
         public static InstanceBehavior FriendsPlusInstanceBehavior
         {
-            get { return friendsPlusInstanceBehavior.Value; }
+            //get { return friendsPlusInstanceBehavior.Value; }
+            get { return (InstanceBehavior)Enum.Parse(typeof(InstanceBehavior), friendsPlusInstanceBehavior.Value); }
         }
         public static InstanceBehavior PublicInstanceBehavior
         {
-            get { return publicInstanceBehavior.Value; }
+            //get { return publicInstanceBehavior.Value; }
+            get { return (InstanceBehavior)Enum.Parse(typeof(InstanceBehavior), publicInstanceBehavior.Value); }
         }
         public static bool IncludeFavoritesList
         {
@@ -41,7 +47,8 @@ namespace PrivateInstanceIcon
         private static readonly string categoryName = "PrivateInstanceIcon Config";
         protected static MelonPreferences_Category category;
 
-        protected static MelonPreferences_Entry<InstanceBehavior> privateInstanceBehavior, joinablePrivateInstanceBehavior, friendsInstanceBehavior, friendsPlusInstanceBehavior, publicInstanceBehavior;
+        // TODO: Once UIX gets proper enum support change this to MelonPreferences_Entry<InstanceBehavior>
+        protected static MelonPreferences_Entry<string> privateInstanceBehavior, joinablePrivateInstanceBehavior, friendsInstanceBehavior, friendsPlusInstanceBehavior, publicInstanceBehavior;
         protected static MelonPreferences_Entry<bool> includeFavoritesList;
 
         public static void Init()
@@ -56,43 +63,18 @@ namespace PrivateInstanceIcon
             publicInstanceBehavior = CreateInstanceBehaviourMapping("Public", InstanceBehavior.Default);
         }
 
-        internal static MelonPreferences_Entry<InstanceBehavior> CreateInstanceBehaviourMapping(string instanceTypeName, InstanceBehavior defaultVal)
+        internal static MelonPreferences_Entry<string> CreateInstanceBehaviourMapping(string instanceTypeName, InstanceBehavior defaultVal)
         {
             string prefName = instanceTypeName + " Instances";
-            var pref = category.CreateEntry(prefName, defaultVal, $"How the list should behave for {instanceTypeName} instances"); ;
-            // The lack of propper enum support in UIX is very very very frustrating.
-            // Some other preferences managers actually properly support enums, so storing the actual preference as a string
-            // would just make the actually good implementations not be able to be utilitized properly.
-            // Thusly this jank double registers the preferences as also strings if UIX is present.
+            var pref = category.CreateEntry(prefName, defaultVal.ToString(), $"How the list should behave for {instanceTypeName} instances"); ;
             if (VRCUtils.IsUIXPresent)
             {
-                MelonLogger.Msg($"Registering UIX pref for {prefName}");
                 var enumPossibleValues = new[] {
                     (nameof(InstanceBehavior.ShowIcon), "Show Icons"),
                     (nameof(InstanceBehavior.Default), "Default"),
                     (nameof(InstanceBehavior.HideUsers), "Hide Users")
                 };
-                var uixPrefName = prefName + "(UIX)";
-                MelonPreferences_Entry<string> uixPref = category.CreateEntry(uixPrefName, pref.Value.ToString(), $"How the list should behave for {instanceTypeName} instances.");
-                uixPref.OnValueChanged += (oldValue, newValue) =>
-                {
-                    if (oldValue != newValue)
-                    {
-                        bool parsingSuccess = System.Enum.TryParse(newValue, out InstanceBehavior parsed);
-                        if (parsingSuccess)
-                        {
-                            MelonLogger.Msg($"Changed instance type enum for {uixPref.DisplayName}, value = {newValue}");
-                            pref.Value = parsed;
-                        }
-                        else MelonLogger.Warning($"Failed to parse instance type enum for {uixPref.DisplayName}, value = {newValue}");
-                    }
-                };
-                pref.OnValueChanged += (oldValue, newValue) =>
-                {
-                    if (oldValue != newValue)
-                        uixPref.Value = newValue.ToString();
-                };
-                ExpansionKitApi.RegisterSettingAsStringEnum(categoryName, uixPrefName, enumPossibleValues);
+                ExpansionKitApi.RegisterSettingAsStringEnum(categoryName, prefName, enumPossibleValues);
             }
 
             return pref;
