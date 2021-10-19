2ec6e5049c4e5269bb27de77132b621d5afc0d2d
loukylor <Loukylor@users.noreply.github.com>
2021-10-19 16:55:37 -0700

    Add text to singlebutton

---
+++ VRChatUtilityKit/Ui/IElementWithText.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+
+namespace VRChatUtilityKit.Ui
+{
+    /// <summary>
+    /// An interface for wrappers that hold elements with text.
+    /// </summary>
+    public interface IElementWithText
+    {
+        /// <summary>
+        /// The text mesh pro component on the element.
+        /// </summary>
+        TextMeshProUGUI TextComponent { get; }
+        /// <summary>
+        /// Gets or sets the text on the element.
+        /// </summary>
+        string Text { get; set; }
+    }
+}
--- VRChatUtilityKit/Ui/SingleButton.cs
+++ VRChatUtilityKit/Ui/SingleButton.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 using VRC.DataModel.Core;
 
@@ -7,12 +8,26 @@ namespace VRChatUtilityKit.Ui
     /// <summary>
     /// A wrapper holding a button.
     /// </summary>
-    public class SingleButton : VRCSelectable
+    public class SingleButton : VRCSelectable, IElementWithText
     {
         /// <summary>
         /// The OnClick of the button.
         /// </summary>
         public Action OnClick { get; set; }
+
+        /// <summary>
+        /// The text mesh pro component on the button.
+        /// </summary>
+        public TextMeshProUGUI TextComponent { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the text on the button.
+        /// </summary>
+        public string Text 
+        { 
+            get => TextComponent.text; 
+            set => TextComponent.text = value; 
+        }
 
         /// <summary>
         /// Creates a new button.
@@ -23,6 +38,7 @@ namespace VRChatUtilityKit.Ui
         /// <param name="gameObjectName">The name of the button's GameObject</param>
         public SingleButton(GameObject parent, Action onClick, Sprite icon, string gameObjectName) : base(parent, UiManager.QMStateController.transform.Find("Container/Window/QMParent/Menu_Dashboard/ScrollRect/Viewport/VerticalLayoutGroup/Buttons_QuickLinks/Button_Worlds").gameObject, icon, gameObjectName)
         {
+            TextComponent = rectTransform.Find("Text_H4").GetComponent<TextMeshProUGUI>();
             OnClick = onClick;
             BindingExtensions.Bind(ButtonComponent, new Action(() => OnClick?.Invoke()));
         }
